a5e7d02b187a631b459cda5c4b60ed5dce89c698
max pc <huber.maxi@gmail.com>
2021-03-22 17:50:39 +0100

    mvc works

--- src/PT15/ControllerBank.java
+++ src/PT15/ControllerBank.java
@@ -1,5 +1,10 @@
 package PT15;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 import java.awt.*;
 
 public class ControllerBank {
@@ -10,5 +15,50 @@ public class ControllerBank {
         this.model = model;
         this.view = view;
         view.lblKonto.textProperty().bind(model.standProperty().asString());
+        view.btnAuszahlen.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                double betrag = 0;
+                try {
+                    betrag = Double.parseDouble(view.txtKonto.getText());
+                }
+                catch (Exception e)
+                {
+                    view.alertBetrag.show();
+                    return;
+                }
+                if(betrag<0)
+                {
+                    view.alertPositiv.show();
+                    return;
+                }
+                if(model.getStand()<betrag)
+                {
+                    view.alertDeckung.show();
+                    return;
+                }
+                model.zahleAus(betrag);
+            }
+        });
+        view.btnEinzahlen.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+               double betrag = 0;
+                try {
+                    betrag = Double.parseDouble(view.txtKonto.getText());
+                }
+                catch (Exception e)
+                {
+                    view.alertBetrag.show();
+                    return;
+                }
+                if(betrag<0)
+                {
+                    view.alertPositiv.show();
+                    return;
+                }
+                model.zahleEin(betrag);
+            }
+        });
     }
 }
--- src/PT15/ViewKonto.java
+++ src/PT15/ViewKonto.java
@@ -10,6 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.Window;
@@ -20,14 +21,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ViewKonto{
-
-    @FXML
     public TextField txtKonto;
-    @FXML
     public Label lblKonto;
-    @FXML
     public Button btnEinzahlen;
-    @FXML
     public Button btnAuszahlen;
 
     private Konto model;
@@ -41,8 +37,15 @@ public class ViewKonto{
         alertBetrag = new Alert(Alert.AlertType.INFORMATION,"Sie müssen einen Betrag eingeben!", ButtonType.OK);
         alertDeckung = new Alert(Alert.AlertType.ERROR,"Ihr Konto weist keine ausreichende Deckung auf!", ButtonType.OK);
         alertPositiv = new Alert(Alert.AlertType.INFORMATION,"Geben Sie einen positiven Betrag ein!", ButtonType.OK);
-        Parent root = FXMLLoader.load(getClass().getResource("/ViewKontoFxml.fxml"));
+        alertBetrag.initStyle(StageStyle.UTILITY);
+        alertDeckung.initStyle(StageStyle.UTILITY);
+        alertPositiv.initStyle(StageStyle.UTILITY);
+        Pane root = FXMLLoader.load(getClass().getResource("/ViewKontoFxml.fxml"));
         Scene scene = new Scene(root);
+        lblKonto = (Label) scene.lookup("#lblKonto");
+        txtKonto = (TextField) scene.lookup("#txtKonto");
+        btnEinzahlen = (Button) scene.lookup("#btnEinzahlen");
+        btnAuszahlen = (Button) scene.lookup("#btnAuszahlen");
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.show();
